277952237a5ba63ec99fcc0fdbe6ad552d34b1a0
Aviad Elya <0527563488a@gmail.com>
2023-01-14 22:15:10 +0200

    start work

---
+++ src/pags/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Pag1 from '../Pag1'
+import img1 from '../../Images/img1'
+
+function Main() {
+  return (
+    <div>
+      <img src={img1}/>
+    </div>
+  )
+}
+
+export default Main
